29c8c8b3242a54d517874d950c444f10fd3eddad
tpubz <taylor.pubins@icloud.com>
2021-03-15 09:25:27 -0600

    Initial Commit

---
+++ DraftHQ2021/DraftHQ2021App.swift
@@ -0,0 +1,17 @@
+//
+//  DraftHQ2021App.swift
+//  DraftHQ2021
+//
+//  Created by Taylor Pubins on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DraftHQ2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
